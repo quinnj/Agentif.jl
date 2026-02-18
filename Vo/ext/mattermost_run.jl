068cd1c1a922f828b9f8f4061cc69eb3790f9fc4
Jacob Quinn <quinn.jacobd@gmail.com>
2026-02-17 19:14:02 -0700

    Update mattermost for ando

--- Vo/ext/mattermost_run.jl
+++ Vo/ext/mattermost_run.jl
@@ -1,8 +1,8 @@
-using Agentif, Vo, Mattermost
+using Agentif, Vo, Mattermost, Telegram, Dates
 
 const MattermostExt = Base.get_extension(Agentif, :AgentifMattermostExt)
 
-Vo.init!()
+Vo.init!(; name="ando")
 Mattermost.with_mattermost(ENV["MATTERMOST_TOKEN"], ENV["MATTERMOST_URL"]) do
     MattermostExt.run_mattermost_bot() do msg
         Vo.evaluate(Vo.get_current_assistant(), msg)
